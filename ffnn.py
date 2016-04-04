022fc0298ff69cdc71f199a54a322a652455d0bc
nzufelt <nicholas.zufelt@gmail.com>
2016-04-04 06:40:31 +0100

    Implement classed ffnn.py

--- ffnn.py
+++ ffnn.py
@@ -4,7 +4,6 @@ Implement a feedforward neural network using theano
 import numpy as np
 import theano
 import theano.tensor as T
-import pandas as pd
 
 class ffnn(object):
     """ A standard (feedforward) neural network, with one hidden layer.
@@ -18,83 +17,103 @@ class ffnn(object):
         n_epochs -- number of training epochs (iterations of gradient descent)
     """
 
-    def __init__(self, n_inputs, n_hidden, n_outputs, n_epochs=20000, reg=.01, alpha=.01):
+    def __init__(self, n_inputs, n_hidden, n_outputs, n_epochs=10000, print_every = 1000, reg=.01, alpha=.01):
         # Need to initialize the parameters to a small, random number
-        n = 1/(np.sqrt(n_inputs * n_outputs * n_hidden))
-        self.n_inputs,self.n_outputs,self.n_hidden,self.reg,self.alpha=n_inputs,n_outputs,n_hidden,reg,alpha
+        n = 1/np.sqrt(n_inputs)
+        self.n_inputs,self.n_hidden,self.n_outputs,self.reg,self.alpha=n_inputs,n_hidden,n_outputs,reg,alpha
         self.epochs = n_epochs
 
         # Part of the class so that they don't get tossed back and forth between methods
         self.X = T.dmatrix('X')
-        self.y = T.dmatrix('y') # should this be a vector?  I'm thinking a one-hotted vector
+        if n_outputs == 1:
+            # We are performing 2-class classification
+            y = T.dvector('y')
+        else:
+            # We are performing multiclass (i.e. > 2) classification
+            y = T.dmatrix('y')
 
         # Weights and biases
-        self.W1 = theano.shared(n*np.random.randn(n_inputs*n_hidden), name='W1')
-        self.W2 = theano.shared(n*np.random.randn(n_hidden*n_outputs), name='W2')
-        self.b1 = theano.shared(n*np.random.randn(n_inputs), name='b1')
-        self.b2 = theano.shared(n*np.random.randn(n_hidden), name='b2')
+        self.W1 = theano.shared(n*np.random.randn(n_inputs,n_hidden), name='W1')
+        self.W2 = theano.shared(n*np.random.randn(n_hidden,n_outputs), name='W2')
+        self.b1 = theano.shared(np.zeros(n_hidden), name='b1')
+        self.b2 = theano.shared(np.zeros(n_outputs), name='b2')
 
-        self.predict = None # will be a theano function
+        """ With selfs?
+        self.z1 = self.x.dot(self.W1)+self.b1
+        self.hidden = T.tanh(self.z1)
+        self.z2 = self.hidden.dot(self.W2) + self.b2
+        if self.n_outputs == 1:
+            self.output = T.nnet.sigmoid(self.z2)
+            self.prediction = self.output > 0.5
+            self.crossent = -self.y.dot(T.log(self.output)) - (1-self.y).dot(T.log(1-self.output))
+        else:
+            self.output = T.nnet.softmax(self.z2)
+            self.prediction = np.argmax(self.output,axis=1)
+            self.crossent = T.nnet.categorical_crossentropy(self.output,self.y)
 
-    def one_hot(self,y):
-        """ Return a one-hot version of the numeral 0-9."""
-        hot = np.zeros(self.n_outputs)
-        hot[y] = 1
-        return hot
-
-    def cost(self):
-        """ Compute the cost function for the current epoch.
-
-        The cost function we will implement is regularized multi-class cross-entropy function, see below.
+        self.cost = self.crossent.sum() + reg*((self.W1**2).sum()+(self.W2**2).sum())
         """
-        total_cost = 0.
-        # Cost for the fit
-        for x,label in zip(self.X,self.y):
-            y_,_ = self.forward(x)
-            # is this the right cost function?  It's different from the one used in NN-from-scratch
-            # in that it penalyzes equally for false positives and negatives
-            total_cost += (sum(-label*np.log(y_) - (1-y)*np.log(1-y_))/self.n_inputs)
+        z1 = self.X.dot(self.W1)+self.b1
+        hidden = T.tanh(z1)
+        z2 = hidden.dot(self.W2) + self.b2
+        if self.n_outputs == 1:
+            output = T.nnet.sigmoid(z2)
+            srediction = output > 0.5
+            crossent = -self.y.dot(T.log(output)) - (1-self.y).dot(T.log(1-output))
+        else:
+            output = T.nnet.softmax(z2)
+            prediction = np.argmax(output,axis=1)
+            crossent = T.nnet.categorical_crossentropy(output,self.y)
 
-        # Cost for the regularization
-        total_cost += self.reg / (2*len(self.X)) * (sum(np.nditer(self.A**2)) + sum(np.nditer(self.B**2)))
-
-        return total_cost
-
-    def fit(X,Y):
-        """ Fit the model.
-
-        Params:
-            X - 2d np.array of training data, rows are samples, columns are
-                features
-            Y - 1d np.array of training labels
-        """
-
-        x = T.matrix('x')
-        y = T.dvector('y')
-
-        # forward prop
-        hidden = T.tanh(x*W1+b1)
-        output = T.nnet.softmax(hidden*W2 + b2)
-        prediction = output > 0.5
-
-        # cost
-        crossent = -y*T.log(output) - (1-y)*T.log(1-output)
-        cost = crossent.mean() + self.reg * (w**2).sum()
+        cost = crossent.sum() + reg*((self.W1**2).sum()+(self.W2**2).sum())
 
         # gradients
         gW1,gb1,gW2,gb2 = T.grad(cost,[W1,b1,W2,b2])
 
-        # build theano functions
-        epoch = theano.function(inputs = [x,y],
-                                outputs = [output, crossent],
-                                updates = ((W1,W1-self.alpha*gW1),
-                                           (b1,b1-self.alpha*gb1),
-                                           (W2,W2-self.alpha*gW2),
-                                           (b2,b2-self.alpha*gb2)))
-        self.predict = theano.function(inputs=[x],outputs=prediction)
+        # build theano function
+        self.epoch = theano.function(inputs = [self.X,self.y],
+                                     outputs = [output, crossent.sum()],
+                                     updates = ((self.W1,self.W1-self.alpha*gW1),
+                                                (self.b1,self.b1-self.alpha*gb1),
+                                                (self.W2,self.W2-self.alpha*gW2),
+                                                (self.b2,self.b2-self.alpha*gb2)))
+        self.predict = theano.function(inputs=[self.X],outputs=prediction)
 
-        # train the model
+    def fit(self,X,y):
+        """ Fit the model.
+
+        Params:
+            X -- 2d np.array of training data, rows are samples, columns are
+                    features
+            y -- in binary classication: a 1d np.array of labels (0 or 1)
+                 in multiclassification: a 2d np.array of one-hot labels
+        """
         for i in range(self.epochs):
-            pred,err = epoch(X, Y) #note X, Y not x, y
-            if i % print_every == 0:
+            pred,err = self.epoch(X,y) # Note: not self.X or self.y
+            if i % self.print_every == 0:
                 print('Error after epoch {}: {}'.format(i,err))
+
+    def check_on_toy_data(self,n_samples):
+        """ Used for debugging."""
+        # generate toy data
+        N = n_samples
+        if self.n_outputs == 1:
+            D = (np.random.randn(N,self.n_inputs),
+                 np.random.randint(size=N,low=0,high=2))
+        else:
+            I = np.identity(self.n_outputs)
+            D = [np.random.randn(N,self.n_inputs)]
+            D.append(np.array([I[i] for i in np.random.randint(size=N,
+                                                               low=0,
+                                                               high=self.n_outputs)]))
+        # check accuracy
+        if n_outputs == 1:
+            preds = predict(D[0]).T[0]
+            wrong = (preds != D[1]).sum()
+        else:
+            I = np.identity(n_outputs)
+            preds = np.array([I[i] for i in predict(D[0])])
+            wrong = (preds != D[1]).sum() / 2                      # note the /2
+
+        score = (N*1.0 - wrong)/N
+        print("Our model made {} errors, for an accuracy of {}".format(wrong, score))
