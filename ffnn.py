d6d19c0b7bc984d8b6034ebfa08e4c40439544a8
Ubuntu <ubuntu@ip-172-31-24-238.eu-west-1.compute.internal>
2016-04-04 06:04:10 +0000

    Debug ffnn.py

--- ffnn.py
+++ ffnn.py
@@ -15,6 +15,7 @@ class ffnn(object):
         reg -- the regularization parameter
         alpha -- the learning rate
         n_epochs -- number of training epochs (iterations of gradient descent)
+        print_every -- print the error after this many epochs
     """
 
     def __init__(self, n_inputs, n_hidden, n_outputs, n_epochs=10000, print_every = 1000, reg=.01, alpha=.01):
@@ -22,15 +23,16 @@ class ffnn(object):
         n = 1/np.sqrt(n_inputs)
         self.n_inputs,self.n_hidden,self.n_outputs,self.reg,self.alpha=n_inputs,n_hidden,n_outputs,reg,alpha
         self.epochs = n_epochs
+        self.print_every = print_every
 
         # Part of the class so that they don't get tossed back and forth between methods
         self.X = T.dmatrix('X')
         if n_outputs == 1:
             # We are performing 2-class classification
-            y = T.dvector('y')
+            self.y = T.dvector('y')
         else:
             # We are performing multiclass (i.e. > 2) classification
-            y = T.dmatrix('y')
+            self.y = T.dmatrix('y')
 
         # Weights and biases
         self.W1 = theano.shared(n*np.random.randn(n_inputs,n_hidden), name='W1')
@@ -68,7 +70,7 @@ class ffnn(object):
         cost = crossent.sum() + reg*((self.W1**2).sum()+(self.W2**2).sum())
 
         # gradients
-        gW1,gb1,gW2,gb2 = T.grad(cost,[W1,b1,W2,b2])
+        gW1,gb1,gW2,gb2 = T.grad(cost,[self.W1,self.b1,self.W2,self.b2])
 
         # build theano function
         self.epoch = theano.function(inputs = [self.X,self.y],
@@ -93,27 +95,33 @@ class ffnn(object):
             if i % self.print_every == 0:
                 print('Error after epoch {}: {}'.format(i,err))
 
-    def check_on_toy_data(self,n_samples):
+    def _generate_toy_data(self,n_samples):
         """ Used for debugging."""
-        # generate toy data
-        N = n_samples
         if self.n_outputs == 1:
-            D = (np.random.randn(N,self.n_inputs),
-                 np.random.randint(size=N,low=0,high=2))
+            D = (np.random.randn(n_samples,self.n_inputs),
+                 np.random.randint(size=n_samples,low=0,high=2))
         else:
             I = np.identity(self.n_outputs)
-            D = [np.random.randn(N,self.n_inputs)]
-            D.append(np.array([I[i] for i in np.random.randint(size=N,
+            D = [np.random.randn(n_samples,self.n_inputs)]
+            D.append(np.array([I[i] for i in np.random.randint(size=n_samples,
                                                                low=0,
                                                                high=self.n_outputs)]))
+        return D
+
+    def check_on_toy_data(self,n_samples):
+        """ Used for debugging."""
+        D = self._generate_toy_data(n_samples)
+
+        self.fit(D[0],D[1])
+
         # check accuracy
-        if n_outputs == 1:
-            preds = predict(D[0]).T[0]
+        if self.n_outputs == 1:
+            preds = self.predict(D[0]).T[0]
             wrong = (preds != D[1]).sum()
         else:
-            I = np.identity(n_outputs)
-            preds = np.array([I[i] for i in predict(D[0])])
+            I = np.identity(self.n_outputs)
+            preds = np.array([I[i] for i in self.predict(D[0])])
             wrong = (preds != D[1]).sum() / 2                      # note the /2
 
-        score = (N*1.0 - wrong)/N
+        score = (n_samples*1.0 - wrong)/n_samples
         print("Our model made {} errors, for an accuracy of {}".format(wrong, score))
