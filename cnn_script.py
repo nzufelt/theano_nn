fba788f32c23ebb5343dc685f59fb3d348776bc3
Ubuntu <ubuntu@ip-172-31-24-238.eu-west-1.compute.internal>
2016-04-06 13:45:46 +0000

    hyperparam tuning

--- cnn_script.py
+++ cnn_script.py
@@ -18,23 +18,23 @@ from theano.tensor.nnet import relu,conv2d
 
 #### Parameters
 # For the whole network
-reg,alpha = .01,.01 #(float(i) for i in sys.argv[4:6])
-minibatch = 200 #(int(i) for i in sys.argv[1:4])
-n_samples = 42000
+reg,alpha = .01,.2 #(float(i) for i in sys.argv[4:6])
+minibatch = 128 #(int(i) for i in sys.argv[1:4])
+n_samples = 2000 - (2000 % minibatch) # make this better!
 n_channels = 1 # greyscale
-epochs,print_every = 10,1#100,10 #(int(i) for i in sys.argv[6:8])
+epochs,print_every = 200,20 #(int(i) for i in sys.argv[6:8])
 # For the convolutional layer
 image_height,image_width = 28,28
-filter_height,filter_width = 3,3 
-pool_size,n_filters = (2,2),1 # n_filters is the number of copies of the filter
+filter_height,filter_width = 5,5 
+pool_size,n_filters = (2,2),3 # n_filters is the number of copies of the filter
 # For the Fully-connected layer
-n_inputs = ((image_height - filter_height + 1) // pool_size[0])**2
+n_inputs = n_filters*((image_height - filter_height + 1) // pool_size[0])**2
 # figure out why above is correct! Note that we assume square everything
-# for MNIST, with 3x3 filter and 2x2 pooling, this gives 13x13=169
-n_hidden,n_outputs = 70,10
+# for MNIST, with 5x5 filter and 2x2 pooling, this gives 13x13=169
+n_hidden,n_outputs = 90,10
 
 # Need to initialize the weights to a small, random number
-n = 1/(np.sqrt(n_inputs))
+n_conv = 2/(np.sqrt(n_filters*filter_height*filter_width))
 
 # Input and training values
 X = T.tensor4(name='X')
@@ -43,33 +43,33 @@ y = T.dmatrix('y') # one-hot vectors
 
 # Convolution layer
 W_shape = (n_filters,n_channels,filter_height,filter_width)
-W_conv = theano.shared(n*rng.randn(*W_shape),
+W_conv = theano.shared(n_conv*rng.randn(*W_shape),
                        name='W_conv')
 conv_out_layer = conv2d(X, 
                         W_conv,
                         input_shape=X_shape,
                         filter_shape=W_shape,
                         border_mode='valid')
-# Note:
+# NOTE:
 # output_shape = (minibatch, 1, output_rows, output_columns)
 
 # Pooling layer
 pooled_out = pool_2d(input=conv_out_layer,
                      ds=pool_size,
                      ignore_border=True)
+# NOTE:
 # ignore_border ==> round down if convolution_output / pool_size is not int
-# OLD: pooled_out = T.signal.downsample.max_pool_2d(input=conv_out,
 
 # Implement the bias term and nonlinearity
 b_conv = theano.shared(np.zeros(n_filters,), name='b_conv')
 conv_out = relu(pooled_out + b_conv.dimshuffle('x',0,'x','x'))
-#conv_out_flat = conv_out.reshape(minibatch,n_inputs)
 conv_out_flat = conv_out.flatten(2)
 
 # Fully-connected layers
-W1_full = theano.shared(n*rng.randn(n_inputs,n_hidden), name='W1_full')
+n_full = 1/(np.sqrt(n_inputs))
+W1_full = theano.shared(n_full*rng.randn(n_inputs,n_hidden), name='W1_full')
 b1_full = theano.shared(np.zeros(n_hidden), name='b1_full')
-W2_full = theano.shared(n*rng.randn(n_hidden,n_outputs), name='W2_full')
+W2_full = theano.shared(n_full*rng.randn(n_hidden,n_outputs), name='W2_full')
 b2_full = theano.shared(np.zeros(n_outputs), name='b2_full')
 
 z1 = conv_out_flat.dot(W1_full) + b1_full
@@ -77,23 +77,25 @@ hidden = relu(z1)
 z2 = hidden.dot(W2_full) + b2_full
 output = T.nnet.softmax(z2)
 prediction = np.argmax(output,axis=1)
-crossent = T.nnet.categorical_crossentropy(output,y)
+crossent = T.nnet.categorical_crossentropy(output,y)/n_samples
 
-cost = crossent.sum() + reg*((W1_full**2).sum()+(W2_full**2).sum())
+cost = crossent.sum() + reg*((W_conv**2).sum()+(W1_full**2).sum()+(W2_full**2).sum())
 
 # gradients and update statements
 params = [W_conv,b_conv,W1_full,b1_full,W2_full,b2_full]
 grads = T.grad(cost,[*params])
-updates = tuple((param,param - alpha * grad) for param, grad in zip(params,grads))
+updates = [(param,param - alpha * grad) for param, grad in zip(params,grads)]
 
 # build theano functions
 epoch = theano.function(inputs = [X,y],
-                        outputs = [output, cost],
+                        outputs = [],
                         updates = updates)
 predict = theano.function(inputs=[X],outputs=prediction)
+compute_cost = theano.function(inputs = [X,y],
+                               outputs = [cost,crossent.sum()])
 
 # Read in MNIST data
-train_df = pd.read_csv('train.csv')
+train_df = pd.read_csv('train.csv')[0:n_samples]
 X_data = train_df.values
 del train_df # free up some memory
 I = np.identity(10)
@@ -101,18 +103,27 @@ y_data = np.array([I[i] for i in X_data.T[0].T]) # one-hot the y's
 # strip off response variable and make into a 4-tensor:
 X_data = np.reshape(X_data.T[1:].T,(n_samples,n_channels,image_height,image_width))  
  
-# Give the data mean 0:
+# Give the data mean 0 and range -1 to 1:
 X_data = X_data.astype(float)
 X_data -= 128.0
+X_data /= 128.0
 
 # train the model
 for i in range(epochs):
     for ind in range(0,n_samples,minibatch):
-        rows = list(range(ind,min(ind+minibatch,n_samples+1)))
-        pred,err = epoch(X_data[rows],y_data[rows])
+        rows = list(range(ind,min(ind+minibatch,n_samples)))
+        epoch(X_data[rows],y_data[rows])
     if i % print_every == 0:
-        #print('Error after epoch {}: {}'.format(i,err))
-        print('Completed epoch ' + str(i))
+        current_cost = 0
+        current_crossent = 0
+        for ind in range(0,n_samples,minibatch):
+            rows = list(range(ind,min(ind+minibatch,n_samples+1)))
+            new_cost,new_crossent = compute_cost(X_data[rows],y_data[rows]) 
+            current_cost += new_cost
+            current_crossent += new_crossent
+        print('Cost after epoch {} is {}, with crossentropy {}'.format(i,
+                                                                    current_cost,
+                                                                    current_crossent))
 
 # Accuracy testing
 wrong = 0
@@ -120,6 +131,6 @@ I = np.identity(n_outputs)
 for ind in range(0,n_samples,minibatch):
     rows = list(range(ind,min(ind+minibatch,n_samples+1)))
     preds = np.array([I[i] for i in predict(X_data[rows])])
-    wrong += (preds != y[rows]).sum() / 2  
+    wrong += (preds != y_data[rows]).sum() / 2  
 score = (n_samples*1.0 - wrong)/n_samples
 print("Our model made {} errors, for an accuracy of {}".format(wrong, score))
